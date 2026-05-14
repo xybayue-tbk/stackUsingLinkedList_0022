4d7b1976b60a09a1638b5f1a0498cdf7aa9aea47
xybayue-tbk <xybayue@gmail.com>
2026-05-14 17:13:16 +0700

    menambahkan isi push

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -23,5 +23,12 @@ public:
         top = NULL;
     }
 
-         int push(int value){}
+         int push(int value){
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push value: " << value << endl;
+        return value;
+         }
 };
