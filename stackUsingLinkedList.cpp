fe42394a35fd5091331cccadf7977bcbea045300
xybayue-tbk <xybayue@gmail.com>
2026-05-14 17:26:30 +0700

    menambahkan switch

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -81,5 +81,10 @@ int main(){
         cout << "4.Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+        }
     }
 }
