c19b064e143b86359b6183605c67baadb72481e6
xybayue-tbk <xybayue@gmail.com>
2026-05-14 17:28:51 +0700

    memberi isi case 4

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -109,6 +109,9 @@ int main(){
                 cout << "Stack is empty.No top value." << endl;
             }
             break;
+        case 4:
+            cout << "Exiting program." << endl;
+            break;
         }
     }
 }
