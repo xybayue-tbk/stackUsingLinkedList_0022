2c885e2125aa06cb58ce74d4d489da3f316379e3
xybayue-tbk <xybayue@gmail.com>
2026-05-14 17:24:55 +0700

    menampilkan

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -74,5 +74,12 @@ int main(){
     int value;
 
     while (choice != 5)
-    {}
+    {
+        cout << "1.push\n";
+        cout << "2.pop\n";
+        cout << "3.peek\n";
+        cout << "4.Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+    }
 }
